93ffb506d98054ce357cb8ca3760a56988c2be4f
Nilsu Duran <duranilsu@gmail.com>
2023-02-22 15:50:43 -0500

    workss

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get "/restaurants", to: "restaurants#index"
+  # get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "/restaurants", to: "restaurants#create"
+  # get '/restaurants/:id', to: "restaurants#show", as: :restaurant
+  # get "/restaurants/:id/reviews/new", to: "restaurants#new_review", as: :new_review
+  # post "/restaurants/:id/reviews", to: "restaurants#show_reviews", as: :restaurant_reviews
+
+
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [:new, :create]
+  end
 end
